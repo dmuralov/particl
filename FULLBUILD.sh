622e4b2635df9e328887683d383dc6f28ac24589
dmuralov <dmpirogkov@mail.ru>
2021-09-15 18:28:29 +0300

    add FULLBUILD.sh

---
+++ FULLBUILD.sh
@@ -0,0 +1,5 @@
+./contrib/install_db4.sh `pwd`
+./autogen.sh
+export BDB_PREFIX='/root/falcon/db4'
+./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include"
+make -j 12
